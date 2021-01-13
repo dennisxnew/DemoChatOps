f17552d1cbfed8a55b459e4245ba79102d6d008f
Dennis.Chen <dennis.chen@hy-tech.com.tw>
2021-01-13 14:00:08 +0800

    init version

---
+++ src/main/java/com/hyt/chatops/InitApp.java
@@ -0,0 +1,20 @@
+package com.hyt.chatops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class InitApp extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(InitApp.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitApp.class, args);
+	}
+
+}
---
+++ src/main/java/com/hyt/chatops/controllers/DemoChatOpsController.java
@@ -0,0 +1,28 @@
+package com.hyt.chatops.controllers;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Dennis.Chen
+ * @Date 2020/01/13
+ */
+@RestController
+@RequestMapping("/demo")
+public class DemoChatOpsController {
+
+    private static Logger log = LogManager.getLogger(DemoChatOpsController.class);
+
+    @RequestMapping("/getTestMessage")
+    public Map<String, String> getTestMessage(){
+        Map<String, String> result = new HashMap<>();
+        result.put("message", "Demo Test Message");
+
+        return result;
+    }
+}
